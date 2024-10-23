72d1db33db1c2d7aa99ad36d59dfbe87333f3607
Francisco Cumsille <jfcumsillei@gmail.com>
2024-10-23 19:57:46 -0300

    feat(book): add url to book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2024_10_23_002252) do
+ActiveRecord::Schema[8.1].define(version: 2024_10_23_225534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[8.1].define(version: 2024_10_23_002252) do
     t.bigint "book_collection_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
     t.index ["book_collection_id", "order"], name: "index_books_on_book_collection_id_and_order", unique: true
     t.index ["book_collection_id"], name: "index_books_on_book_collection_id"
   end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,6 +8,7 @@
 #  book_collection_id :integer          not null
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
+#  url                :string
 #
 # Indexes
 #
