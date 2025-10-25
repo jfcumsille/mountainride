74aff1c89c2d16ea224c1a0acc8cfdd4ed9bbc3e
Juan Francisco Cumsille <jfcumsillei@gmail.com>
2025-10-25 09:55:57 -0300

    clean new beginning

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2024_10_23_225534) do
+ActiveRecord::Schema[8.2].define(version: 2024_10_21_014301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
     t.bigint "author_id"
+    t.string "author_type"
+    t.text "body"
     t.datetime "created_at", null: false
+    t.string "namespace"
+    t.bigint "resource_id"
+    t.string "resource_type"
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
@@ -29,41 +29,14 @@ ActiveRecord::Schema[8.1].define(version: 2024_10_23_225534) do
   end
 
   create_table "admin_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
-
-  create_table "authors", force: :cascade do |t|
-    t.string "real_name", null: false
-    t.string "public_name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "book_collections", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_book_collections_on_name", unique: true
-  end
-
-  create_table "books", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "order", null: false
-    t.bigint "book_collection_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "url"
-    t.index ["book_collection_id", "order"], name: "index_books_on_book_collection_id_and_order", unique: true
-    t.index ["book_collection_id"], name: "index_books_on_book_collection_id"
-  end
-
-  add_foreign_key "books", "book_collections"
 end
